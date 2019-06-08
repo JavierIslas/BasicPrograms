655d08bda18fed60834c67613dd3666c1d506f30
Javier Islas <Dimcairion@live.com>
2019-06-08 17:27:56 -0300

    Levels, lifes and Modules added

--- GuessTheCode.cpp
+++ GuessTheCode.cpp
@@ -1,26 +1,76 @@
 #include <iostream>
+#include <ctime>
+
+void Initialize(int* SUMptr, int* MULptr, int Difficulty)
+{
+    int a = rand() % Difficulty + Difficulty;
+    int b = rand() % Difficulty + Difficulty;
+    int c = rand() % Difficulty + Difficulty;
+    *SUMptr = a + b + c;
+    *MULptr = a * b * c;
+}
+
+void Introduction(int L, int S, int M)
+{
+    std::cout << "Guess The Level " << L << " Code:" << std::endl;
+    std::cout << "The sum of the 3 digits on the number is: " << S << std::endl;
+    std::cout << "The multiplication of the 3 digits on the number is :" << M << std::endl;
+    std::cout << "Enter the Code: ";
+}
+
+void ReadInput(int* inSUMptr, int* inMULptr)
+{
+    int inputA, inputB, inputC;
+    std::cin >> inputA >> inputB >> inputC;
+    *inSUMptr = inputA + inputB + inputC;
+    *inMULptr = inputA * inputB * inputC;
+}
+
+bool CheckCondition(int inSum, int inMul, int S, int M)
+{
+    if (inSum == S && inMul == M)
+    {
+        std::cout << "Correct" << std::endl;
+        return true;
+    }
+    else
+    {
+        std::cout << "Wrong. " << std::endl;
+        return false;
+    }
+}
+
+bool GameLoop(int Difficulty)
+{
+    int sum, mul, inputSum, inputMul;
+    Initialize(&sum, &mul, Difficulty);
+    Introduction(Difficulty-1, sum, mul);
+    ReadInput(&inputSum, &inputMul);
+    if(CheckCondition(inputSum, inputMul, sum, mul))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 int main()
 {
-    int a = rand() % 9 + 1;
-    int b = rand() % 9 + 1;
-    int c = rand() % 9 + 1;
-    int sum = a + b + c;
-    int mul = a * b * c;
-    std::cout << "Guess The Code:" << std::endl;
-    std::cout << "The sum of the 3 digits on the number is: " << sum << std::endl;
-    std::cout << "The multiplication of the 3 digits on the number is :" << mul << std::endl;
-    int resultA;
-    int resultB;
-    int resultC;
-    std::cin >> resultA;
-    std::cin >> resultB;
-    std::cin >> resultC;
-    if (resultA == a && resultB == b && resultC == c)
-    {
-        std::cout << "Correct";
-    }
-    else std::cout << "Wrong";
-
+    int Level = 2;
+    int Life = 3;
+    srand(time(NULL));
+    while (Life)
+        if (GameLoop(Level))
+        {
+            ++Level;
+        }
+        else
+        {
+            std::cout << "Lifes = " << --Life << std::endl;
+        }
+        
+    std::cout << "Max level reached: " << Level-1;
     return 0;
 }
